92f7b028a2c521fca867ee8f74e35210f1a342e7
Jatin-1111 <off.jatin1111@gmail.com>
2024-11-24 00:01:10 +0530

    clone

--- script.js
+++ script.js
@@ -10,19 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error("localStorage is not available:", error);
     }
 });
-
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    
-    hamburger.classList.toggle('open');
-});
-
-navLinks.addEventListener('click', (e) => {
-    if (e.target.tagName === 'A') {
-        navLinks.classList.remove('active');
-        hamburger.classList.remove('open');
-    }
-});
